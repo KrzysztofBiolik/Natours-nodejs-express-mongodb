4469d6c7bf451c2e5998148d194803de34bf5953
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-13 22:36:16 +0100

    Added custom validator to priceDiscount field

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,9 @@ const tourSchema = new mongoose.Schema(
       type: String,
       require: [true, 'A tour must have a name'], //second argument is error
       unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have maximum 40 characters'],
+      minlength: [10, 'A tour name must have minimum 10 characters'],
     },
     slug: String,
     duration: {
@@ -20,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult ',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -33,7 +42,17 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       require: true,
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (value) {
+          // this only points to current doc on NEW document creation/
+          // not updating
+          return value < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
