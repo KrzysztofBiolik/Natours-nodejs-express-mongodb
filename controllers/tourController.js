38d893d14a86896a094beb27e954de472bb0abda
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-13 10:51:14 +0100

    Added advanced filtering for values of params like less, lessthan, greater, greaterthan

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,13 +3,20 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     console.log(req.query, queryObj);
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
     // EXECUTE QUERY
     const tours = await query;
     // SEND RESPONSE
@@ -20,7 +27,7 @@ exports.getAllTours = async (req, res) => {
         tours: tours,
       },
     });
-  } catch {
+  } catch (err) {
     res.status(404).json({
       status: 'Failes',
       message: err,
