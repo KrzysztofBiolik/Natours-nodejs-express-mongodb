7e15a650aa351f3a7cfd29882dbb974e21f19db8
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-13 13:08:44 +0100

    Created alias for top-5-cheap tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
@@ -38,7 +45,7 @@ exports.getAllTours = async (req, res) => {
     const page = req.query.page * 1 || 1;
     const limit = req.query.limit * 1 || 100;
     const skip = (page - 1) * limit;
-    
+
     query = query.skip(skip).limit(limit);
 
     if (req.query.page) {
