f353b368f97f7aa1dfb31c81f90d550d0bcff2b7
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-14 13:59:48 +0100

    Added custom validator to priceDiscount field

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have maximum 40 characters'],
       minlength: [10, 'A tour name must have minimum 10 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain charaacters'],
     },
     slug: String,
     duration: {
