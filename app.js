f34e5faa978c957973e63449f7583d25990ecf1a
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-14 14:01:29 +0100

    Hadled unhandled routes

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARE
+// 1) MIDDLEWARES
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
 // expres.json() to middleware, czyli funckja, która może
 // modyfikować przychodzące zapytanie o dane.
+app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -33,5 +29,17 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/user', userRouter);
+
+// all służy do wszystkich metod gttp (get,post...)
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl}`,
+  // });
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
