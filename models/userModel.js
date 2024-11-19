f97d90b808783877b320faf5fa5e5124814744fd
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-19 10:38:52 +0100

    Added function genereting resetToken for user who forgot password

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -39,6 +40,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpired: Date,
 });
 
 // document middleware for encryption
@@ -71,6 +74,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   //False means NOT CHanged
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpired = Date.now() + 2 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
