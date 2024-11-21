4b32f6facd8bf43152c48d85a96f82734792ac88
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-21 23:22:59 +0100

    Added getMe endpoint to get data about current login user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,12 +17,17 @@ router.patch(
   authController.updatePassword,
 );
 
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser,
+);
+
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
-router
-  .route('/')
-  .get(userController.getAllUsers)
+router.route('/').get(userController.getAllUsers);
 router
   .route('/:id')
   .get(userController.getUser)
