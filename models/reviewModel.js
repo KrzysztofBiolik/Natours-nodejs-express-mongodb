65c3390b3b2e92ada4bd328f392388d3b3e1fe44
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-21 13:59:54 +0100

    Created virtual properties in TourSchema in reference to reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -36,9 +36,9 @@ const reviewSchema = new mongoose.Schema(
 
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
-    path: 'tour',
-    select: 'name'
-  }).populate({
+    //     path: 'tour',
+    //     select: 'name'
+    //   }).populate({
     path: 'user',
     select: 'name photo',
   });
