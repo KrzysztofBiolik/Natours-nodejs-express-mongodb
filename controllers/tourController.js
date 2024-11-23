a44de44ce49ca3e8ee6843e13f6b1e361054b459
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-23 01:49:23 +0100

    Calculated distance between two points using geospatial aggregation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -116,3 +116,41 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.00621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(new AppError('Please provide latlng in the format lat,lng.'));
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      // jedyny geospatial stage jaki istnieje, zawsze pierwszy
+      // przujmue co najmniej 1 geospatial index
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier, // convert to km/miles
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
+});
