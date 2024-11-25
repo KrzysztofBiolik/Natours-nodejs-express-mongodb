08781dcec399039edb9e715283cf70890a1e411a
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-25 13:23:33 +0100

    Extended base template with tour and overview Blocks

--- app.js
+++ app.js
@@ -73,7 +73,22 @@ app.use((req, res, next) => {
 //middleware "tourRouter chcemy używać na takim route"
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
