04205ac605d51f07b9348a3869e85b4eb5de24d6
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-26 14:32:35 +0100

    Setted up zoomControl, scroolWheelZoom map options to false and bindPopup autoclose option to false

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,26 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   const locationsData = JSON.parse(
-      document.getElementById('map').dataset.locations
+    document.getElementById('map').dataset.locations,
   );
 
   const [longStart, latStart] = locationsData[0].coordinates;
 
-  const map = L.map('map').setView([latStart, longStart], 8);
+  const map = L.map('map', {
+    zoomControl: false,
+    scrollWheelZoom: false,
+  }).setView([latStart, longStart], 8);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
   for (i = locationsData.length - 1; i >= 0; i--) {
-      const currLocation = locationsData[i];
+    const currLocation = locationsData[i];
 
-      const [long, lat] = currLocation.coordinates;
+    const [long, lat] = currLocation.coordinates;
 
-      const marker = L.marker([lat, long]).addTo(map);
+    const marker = L.marker([lat, long]).addTo(map);
 
-      marker
-          .bindPopup(`<h1>Arrive on Day ${currLocation.day}</h1><br><h1>Location: ${currLocation.description}.</h1>`).openPopup();
+    marker
+      .bindPopup(
+        `<h1>Arrive on Day ${currLocation.day}</h1><br><h1>Location: ${currLocation.description}.</h1>`,
+        {
+          autoClose: false,
+        },
+      )
+      .openPopup();
   }
 });
