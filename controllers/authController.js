fcb1e227d00cc76de315a371b9f7bc95fbd5503e
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-11-27 01:03:30 +0100

    Created isLoggedIn controller added befrore every viewRoutes, added displaying different data in the header depending on whether the user is logged in or not

--- controllers/authController.js
+++ controllers/authController.js
@@ -109,6 +109,32 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
+// only for render pages, no error
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // 1) Verify token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET,
+    );
+
+    //2 Check is user still exists
+    const freshUser = await User.findById(decoded.id);
+    if (!freshUser) {
+      return next();
+    }
+    //3) Check if user changed password after the token was issued
+    // iat = issued at
+    if (freshUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+    // THERE IS A LOGGED IN USER
+    res.locals.user = freshUser;
+    return next();
+  }
+  next();
+});
+
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     // roles - ["admin", "lead-guide"] role=user
--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,4 @@
 const login = async (email, password) => {
-  console.log(email, password);
   try {
     const res = await axios({
       method: 'POST',
@@ -9,9 +8,14 @@ const login = async (email, password) => {
         password: password,
       },
     });
-    console.log(res);
+
+    if (res.data.status === 'success') {
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
   } catch (err) {
-    console.log(err.response.data);
+    alert(err.response.data.message);
   }
 };
 
