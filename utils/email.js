535fc58c1376da63fa7681910951e9f5e76b8914
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-12-03 21:17:58 +0100

    Added sending of email for password reset

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
-const {convert} = require('html-to-text');
+const { convert } = require('html-to-text');
 
 module.exports = class Email {
   constructor(user, url) {
@@ -28,14 +28,11 @@ module.exports = class Email {
 
   async send(template, subject) {
     // 1) Render HTML based on pug template
-    const html = pug.renderFile(
-      `${__dirname}/../views/email/${template}.pug`,
-      {
-        firstName: this.firstName,
-        url: this.url,
-        subject,
-      },
-    );
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
     // 2) Define email options
     const mailOptions = {
       from: this.from,
@@ -53,5 +50,11 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('Welcome', 'Welcome to the Natours Family');
   }
-};
 
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)',
+    );
+  }
+};
